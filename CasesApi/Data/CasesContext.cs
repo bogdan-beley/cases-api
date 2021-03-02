af89c8af5e61f3a96368d9be5b25662a77a11ebc
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-02 21:49:59 +0200

    Added CasesContext

---
+++ CasesApi/Data/CasesContext.cs
@@ -0,0 +1,17 @@
+﻿using CasesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasesApi.Data
+{
+    public class CasesContext : DbContext
+    {
+        public CasesContext(DbContextOptions<CasesContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Incident> Incidents { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+    }
+}
