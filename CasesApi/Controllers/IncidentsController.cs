59264a8c005835742657486cdca3d615e28d3147
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-02 19:53:20 +0200

    Added IncidentsController
    Implemented GetAllIncidentsAsync and GetIncidentByNameAsync methods

---
+++ CasesApi/Controllers/IncidentsController.cs
@@ -0,0 +1,48 @@
+﻿using CasesApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class IncidentsController : ControllerBase
+    {
+        private readonly IIncidentService _incidentService;
+
+        public IncidentsController(IIncidentService incidentService)
+        {
+            _incidentService = incidentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllIncidentsAsync()
+        {
+            var incidents = await _incidentService.GetIncidentsAsync();
+
+            if (incidents == null)
+            {
+                return NotFound("Sorry, the list of incidents is empty.");
+            }
+
+            return Ok(incidents);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetIncidentByNameAsync(string name)
+        {
+            var incident = await _incidentService.GetIncidentByNameAsync(name);
+
+            if (incident == null)
+            {
+                return NotFound($"Sorry. No cases were found for the specified name: {name}. ");
+            }
+
+            return Ok(incident);
+        }
+    }
+}
