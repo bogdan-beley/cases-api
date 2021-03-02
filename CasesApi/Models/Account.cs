d4667faab6a1b100f2669a2a139cf2eed43a288c
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-02 15:40:48 +0200

    Added entity date models

---
+++ CasesApi/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CasesApi.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // unique
+
+        public string IncidentName { get; set; } // is it necessary?
+        public Incident Incident { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
---
+++ CasesApi/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasesApi.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; } // unique identifier
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+}
