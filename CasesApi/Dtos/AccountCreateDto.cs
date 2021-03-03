626df5469ce4073a770a84a0b601e3bfe8f9ecda
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-03 20:09:08 +0200

    Added AccountCreateDto

---
+++ CasesApi/Dtos/AccountCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CasesApi.Dtos
+{
+    public class AccountCreateDto
+    {
+        [Required(ErrorMessage = "'Name' is required")]
+        public string Name { get; set; }
+
+        [ForeignKey("FK_Accounts_Incidents_IncidentName")]
+        public string IncidentName { get; set; }
+    }
+}
