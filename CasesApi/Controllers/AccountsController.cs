c5e6a0928063f15dca7aecc70dca7ac04c6ffe36
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-03 11:56:29 +0200

    Implemented AutoMapper in AccountsController

--- CasesApi/Controllers/AccountsController.cs
+++ CasesApi/Controllers/AccountsController.cs
@@ -3,6 +3,9 @@ using CasesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using CasesApi.Dtos;
+using System.Collections.Generic;
 
 namespace CasesApi.Controllers
 {
@@ -11,14 +14,16 @@ namespace CasesApi.Controllers
     public class AccountsController : ControllerBase
     {
         private readonly IAccountRepo _accountRepo;
+        private readonly IMapper _mapper;
 
-        public AccountsController(IAccountRepo accountRepo)
+        public AccountsController(IAccountRepo accountRepo, IMapper mapper)
         {
             _accountRepo = accountRepo;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllAccountsAsync()
+        public async Task<ActionResult<IEnumerable<AccountReadDto>>> GetAllAccountsAsync()
         {
             var accounts = await _accountRepo.GetAllAccountsAsync();
 
@@ -27,11 +32,11 @@ namespace CasesApi.Controllers
                 return NotFound("Sorry, the list of accounts is empty.");
             }
 
-            return Ok(accounts);
+            return Ok(_mapper.Map<IEnumerable<AccountReadDto>>(accounts));
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetAccountByIdAsync(int id)
+        public async Task<ActionResult<AccountReadDto>> GetAccountByIdAsync(int id)
         {
             var account = await _accountRepo.GetAccountByIdAsync(id);
 
@@ -40,7 +45,7 @@ namespace CasesApi.Controllers
                 return NotFound($"Sorry. No account found for the specified id: {id}. ");
             }
 
-            return Ok(account);
+            return Ok(_mapper.Map<AccountReadDto>(account));
         }
 
         [HttpPost]
