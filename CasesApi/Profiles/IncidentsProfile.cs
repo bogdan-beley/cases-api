e2abb7609b3963c79cec377084bb2ade3afcb70f
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-03 11:10:05 +0200

    Added IncidentReadDto and IncidentsProfile

---
+++ CasesApi/Profiles/IncidentsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CasesApi.Dtos;
+using CasesApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasesApi.Profiles
+{
+    public class IncidentsProfile : Profile
+    {
+        public IncidentsProfile()
+        {
+            CreateMap<Incident, IncidentReadDto>();
+        }
+    }
+}
