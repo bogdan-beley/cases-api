ee578b7a5c8c1a2c415cb33a9deab145d1df8428
Bogdan Beley <56226948+bogdan-beley@users.noreply.github.com>
2021-03-04 16:50:14 +0200

    Update SqlIncidentRepo.cs
    Added comments to PostIncidentAsync method

--- CasesApi/Data/SqlIncidentRepo.cs
+++ CasesApi/Data/SqlIncidentRepo.cs
@@ -28,32 +28,42 @@ namespace CasesApi.Data
 
         public async Task<bool> PostIncidentAsync(Incident incident)
         {
+            // check if incident isn`t null
             if (incident == null)
                 throw new ArgumentNullException(nameof(incident));
 
+            // get account model from incident model
             var account = incident.Accounts.First();
+            // get contact model from incident model
             var contact = incident.Accounts.First().Contacts.First();
+            // check if account with specified name exists in database
             var existingAccount = await _context.Accounts.FirstOrDefaultAsync(a => a.Name == account.Name);
             
+            // if account with specified name does not exist throw exception
             if (existingAccount == null)
                 throw new ArgumentException($"The specified account '{account.Name}' is not found in the database.");
 
+            // check if contact with specified email exists in database
             var existingContact = await _context.Contacts.FirstOrDefaultAsync(c => c.Email == contact.Email);
 
+            // if contact exists update first and last name
             if (existingContact != null)
             {
                 existingContact.FirstName = contact.FirstName;
                 existingContact.LastName = contact.LastName;
 
+                // link contact record to account if not
                 if (existingContact.AccountId == null)
                     existingContact.AccountId = existingAccount.Id;
             }
+            // if contact does not exist in database link to account and add to database
             else
             {
                 contact.AccountId = existingAccount.Id;
                 await _context.Contacts.AddAsync(contact);
             }
             
+            // clear incident accounts before adding to context (because we account is already in database)
             incident.Accounts.Clear();
             await _context.Incidents.AddAsync(incident);
 
