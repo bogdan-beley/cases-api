599d9e2b9ce5412aac114e006648dbe25fe37928
Bogdan Beley <bogdan.beley@outlook.com>
2021-03-07 22:01:39 +0200

    Removed unnecessary usings

--- CasesApi/Dtos/AccountReadDto.cs
+++ CasesApi/Dtos/AccountReadDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CasesApi.Dtos
+﻿namespace CasesApi.Dtos
 {
     public class AccountReadDto
     {
--- CasesApi/Dtos/IncidentCreateDto.cs
+++ CasesApi/Dtos/IncidentCreateDto.cs
@@ -1,9 +1,5 @@
-﻿using CasesApi.Models;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CasesApi.Dtos
 {
